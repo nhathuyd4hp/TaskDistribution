8da5ad0f38f7754e486d0ce47b782b115c45e659
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2025-12-25 11:32:03 +0700

    Refactor HomePage

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -2,12 +2,12 @@ import 'package:fluent_ui/fluent_ui.dart';
 import "package:local_notifier/local_notifier.dart";
 import 'package:provider/provider.dart';
 import 'package:task_distribution/core/widget/header.dart';
+import "package:task_distribution/provider/page.dart";
 import "package:task_distribution/provider/socket.dart";
 import "package:task_distribution/view/log/log.dart";
-import "../provider/page.dart";
-import "../view/robot/robot.dart";
-import "../view/schedule/schedule.dart";
-import "../view/run/runs.dart";
+import "package:task_distribution/view/robot/robot.dart";
+import "package:task_distribution/view/run/runs.dart";
+import "package:task_distribution/view/schedule/schedule.dart";
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -17,27 +17,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Biến để lưu reference provider nhằm remove listener khi dispose
   late ServerProvider _serverProvider;
 
   @override
   void initState() {
     super.initState();
-    // 1. Đăng ký lắng nghe sự kiện khi widget được tạo
     _serverProvider = context.read<ServerProvider>();
     _serverProvider.addListener(_onServerChanged);
   }
 
   @override
   void dispose() {
-    // 2. Hủy lắng nghe khi widget bị hủy (tránh memory leak)
     _serverProvider.removeListener(_onServerChanged);
     super.dispose();
   }
 
-  // --- HÀM XỬ LÝ SIDE EFFECTS (Thông báo, Dialog...) ---
   void _onServerChanged() {
-    // Kiểm tra mounted để đảm bảo widget còn tồn tại
     if (!mounted) return;
 
     final server = _serverProvider;
@@ -47,7 +42,7 @@ class _HomeState extends State<Home> {
       final msg = server.errorMessage!;
 
       // Show Notification
-      _showLocalNotification("Lỗi", msg);
+      _showLocalNotification("ERR", msg);
 
       // Show InfoBar
       _showInfoBar(msg, InfoBarSeverity.error);
@@ -60,7 +55,7 @@ class _HomeState extends State<Home> {
     if (server.latestMessage != null) {
       final msg = server.latestMessage!;
 
-      _showLocalNotification("Thông báo", msg);
+      _showLocalNotification("INFO", msg);
       _showInfoBar(msg, InfoBarSeverity.info);
 
       server.clearLatestMessage();
@@ -83,10 +78,10 @@ class _HomeState extends State<Home> {
           title: Text(severity == InfoBarSeverity.error ? 'Error' : 'Info'),
           content: Text(message),
           severity: severity,
-          onClose: close, // Cho phép user đóng thủ công
+          onClose: close,
         );
       },
-      duration: const Duration(seconds: 3), // Tự tắt sau 3s
+      duration: const Duration(seconds: 3),
     );
   }
 
